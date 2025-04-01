b69ce4dbcfcf2b2c6aef1a0c64d93367af9f21f9
rvv <5573352@gmail.com>
2025-04-01 13:04:43 +0300

    Initial Commit

---
+++ PythagorasSquare/PythagorasSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PythagorasSquareApp.swift
+//  PythagorasSquare
+//
+//  Created by Roman Valchuk on 01.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PythagorasSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
